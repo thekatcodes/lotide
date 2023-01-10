55682a662e04e053f27a3ea8ce1efc49521d6689
Katie Liu <katieliu59@gmail.com>
2023-01-09 23:48:01 -0500

    Implement head to retrieve the first element from an array and refactor assertEqual code

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+	let a = actual;
+	let e = expected;
+	if (actual === expected) {
+		console.log(`😊 Assertion Passed: ${a} === ${e}`);
+	} else if (actual !== expected) {
+		console.log(`☹️ Assertion Failed: ${a} !== ${e}`);
+	}
+	const head = function(array) {
+        return array[0];
+      };
+    
+};
+
+const head = function(array) {
+    return array[0];
+  };
+
+//TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
