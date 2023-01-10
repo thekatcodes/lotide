1c77347a657b0f0faf1adca28baae7a1220737be
Katie Liu <katieliu59@gmail.com>
2023-01-09 23:17:03 -0500

    Implement console.assert

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+	if (actual === expected) {
+		console.log("😊 Assertion Passed: " + actual + " === " + expected);
+	} else if (actual !== expected) {
+		console.log();
+	}
+	console.log("☹️ Assertion Failed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
