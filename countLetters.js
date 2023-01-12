5adadd2fd9530c833305d06cf7ae924031f2bd72
Katie Liu <katieliu59@gmail.com>
2023-01-11 20:17:55 -0500

    Import assertEqual function

---
+++ countLetters.js
@@ -0,0 +1,7 @@
+const assertEqual = function (actual, expected) {
+	if (actual === expected) {
+		console.log(`😊 Assertion Passed: ${actual} === ${expected}`);
+	} else if (actual !== expected) {
+		console.log(`☹️ Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
