03e314dd574b87f5404f19e8ff2cd5b5a6b4105f
Katie Liu <katieliu59@gmail.com>
2023-01-12 22:17:48 -0500

    Implement function to find and return first element when predicate returns truthy

---
+++ findKey.js
@@ -0,0 +1,20 @@
+const findKey = function (obj, callback) {
+	// scan object
+	for (let key in obj) {
+		if (callback(obj[key])) {
+			return key;
+		}
+	}
+	//return first key for which callback returns a truthy value
+	//else return undefined
+	return undefined;
+};
+
+// findKey({
+//     "Blue Hill": { stars: 1 },
+//     "Akaleri":   { stars: 3 },
+//     "noma":      { stars: 2 },
+//     "elBulli":   { stars: 3 },
+//     "Ora":       { stars: 2 },
+//     "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2) // => "noma"
