75a7328290824f0d045dedb30ae67a1b76570392
Katie Liu <katieliu59@gmail.com>
2023-01-16 16:52:15 -0500

    Refactor countLetters function and correct syntax

--- countLetters.js
+++ countLetters.js
@@ -1,27 +1,26 @@
 const assertEqual = require("./assertEqual");
 
-const countLetters = function (sentence) {
-	let noSpaceSentence = sentence.replace(/\s/g, "");
-	let letterCount = {};
-	// loop through each letter of the string
-	for (let letter of noSpaceSentence) {
-		// console.log(letter); -> logs each letter of the sentence individualy
-		// if (!Object.hasOwn(letterCount, letter)){
-		//     letterCount[letter] = 1;
-		// } else {
-		//     letterCount[letter]++;
-		// } -> refactor for older node version (old version currently installed doesn't recognice hasOwn)
-		for (const letter of noSpaceSentence) {
-			if (letterCount[letter]) {
-				letterCount[letter] += 1;
-			} else {
-				letterCount[letter] = 1;
-			}
-		}
-		// console.log(letterCount);
-		// return a count of each of the letters in the sentence
-		return letterCount;
-	}
+const countLetters = function(sentence) {
+    let noSpaceSentence = sentence.replace(/\s/g, "");
+    let letterCount = {};
+    // loop through each letter of the string
+    // for (let letter of noSpaceSentence) {
+    // console.log(letter); -> logs each letter of the sentence individualy
+    // if (!Object.hasOwn(letterCount, letter)){
+    //     letterCount[letter] = 1;
+    // } else {
+    //     letterCount[letter]++;
+    // } -> refactor for older node version (old version currently installed doesn't recognice hasOwn)
+    for (const letter of noSpaceSentence) {
+        if (letterCount[letter]) {
+            letterCount[letter] += 1;
+        } else {
+            letterCount[letter] = 1;
+        }
+    }
+    // console.log(letterCount);
+    // return a count of each of the letters in the sentence
+    return letterCount;
 };
 
 module.exports = countLetters;
