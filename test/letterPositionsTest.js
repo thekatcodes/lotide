c7039b5b6cd16fd0f519284f38d7806933bfa25f
Katie Liu <katieliu59@gmail.com>
2023-01-16 17:28:50 -0500

    Correct syntax

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -4,7 +4,7 @@ const letterPositions = require("../letterPositions");
 describe("#letterPosition", () => {
     const sentence1 = letterPositions('hello');
 
-;	it("should return [1] for hello.e", () => {
+	it("should return [1] for hello.e", () => {
 		assert.deepEqual(sentence1['e'], [1]);
 	});
 });
