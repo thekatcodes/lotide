3b7eaaa2e1ed8595c9d15fde568430db54b470d0
Katie Liu <katieliu59@gmail.com>
2023-01-16 16:02:02 -0500

    Add functions to one object so taht we can export this object containing all our functions

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const eqArrays = require("../assertEqual");
+const assertEqual = require("../assertEqual");
+const assertArraysEqual = require("../assertArraysEqual");
+
+module.exports = {
+	head: head,
+	tail: tail,
+	middle: middle,
+	eqArrays: eqArrays,
+	assertEqual: assertEqual,
+	assertArraysEqual: assertArraysEqual,
+};
