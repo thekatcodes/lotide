25fdf9d5649489345878a6a1212f1b5dd3a51041
Katie Liu <katieliu59@gmail.com>
2023-01-16 17:44:37 -0500

    Refactor takeUntil and export to takeUntilTest to use Mocha and Chai for testing

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const middle = require("./middle");
 const eqArrays = require("../eqArrays");
 const assertEqual = require("../assertEqual");
 const assertArraysEqual = require("../assertArraysEqual");
-const eqObjects = require('../eqObjects');
-
+const eqObjects = require("../eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
 
 module.exports = {
 	head: head,
@@ -14,6 +17,10 @@ module.exports = {
 	middle: middle,
 	eqArrays: eqArrays,
 	assertEqual: assertEqual,
-    assertArraysEqual: assertArraysEqual,
-    eqObjects: eqObjects
+	assertArraysEqual: assertArraysEqual,
+	eqObjects: eqObjects,
+	findKey: findKey,
+	findKeyByValue: findKeyByValue,
+	letterPositions: letterPositions,
+	map: map,
 };
