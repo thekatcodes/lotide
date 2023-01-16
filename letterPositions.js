a1704a7655817b5a3608b6f581f3f746b134c44d
Katie Liu <katieliu59@gmail.com>
2023-01-16 17:27:57 -0500

    Refactor letterPositions and export to letterPositionsTest to use Mocha and Chai for testing

--- letterPositions.js
+++ letterPositions.js
@@ -1,44 +1,18 @@
-function assertArraysEqual(arr1, arr2) {
-	let isGood = true;
-	//check if length of arrays are equal
-	if (arr1.length !== arr2.length) {
-		isGood = false;
-	}
-	//loop through the array content to compare if they're true
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr1[i] !== arr2[i]) {
-			isGood = false;
-		}
-	}
-	//what to return if isGood is true or false
-	if (isGood) {
-		console.log("Arrays are the same");
-	} else {
-		console.log("Arrays are not the same...");
-	}
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
-const letterPositions = function (sentence) {
-	/* 
- 1) remove spaces from string
- 2) loop through string
- 3) for each letter create an array
- 4) push number to the array 
- */
-
-	const results = {};
-	let noSpaceSentence = sentence.replace(/\s/g, "");
-
-	for (let i = 0; i < noSpaceSentence; i++) {
-        if (results[noSpaceSentence[i]]){
-            results[noSpaceSentence[i]].push(i); //push(i) with() adds to the array
-        } else {
-            results[noSpaceSentence[i]]= [i]; //[i] creates an array with the index
+const letterPositions = function(sentenceAsString) {
+    const resultsObject = {};
+    for (let i = 0; i < sentenceAsString.length; i++) {
+      let currentChar = sentenceAsString[i];
+      if (currentChar !== " ") {
+        if (!resultsObject[currentChar]) {
+          resultsObject[currentChar] = [i];
+        } else if (resultsObject[currentChar]) {
+          resultsObject[currentChar].push(i);
         }
-	}
-	return results;
+      }
+    }
+    return resultsObject;
+  };
 
-};
-
-// TEST
-letterPositions("hello").e, [1];
+module.exports = letterPositions;
---
+++ test/letterPositionsTest.js
@@ -0,0 +1,15 @@
+const assert = require("chai").assert;
+const letterPositions = require("../letterPositions");
+
+describe("#letterPosition", () => {
+    const sentence1 = letterPositions('hello');
+
+;	it("should return [1] for hello.e", () => {
+		assert.deepEqual(sentence1['e'], [1]);
+	});
+});
+
+
+
+// TEST
+letterPositions("hello").e, [1];
