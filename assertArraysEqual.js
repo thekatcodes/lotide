1a7c75f493485f761de9e80ed73f9579c5e6ce7f
Katie Liu <katieliu59@gmail.com>
2023-01-16 15:02:49 -0500

    Refactor code to store test code in separate files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,10 @@
-function assertArraysEqual(arr1, arr2) {
-	let isGood = true;
-//check if length of arrays are equal
-	if (arr1.length !== arr2.length) {
-		isGood = false;
-	}
-//loop through the array content to compare if they're true
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr1[i] !== arr2[i]) {
-			isGood = false;
-		}
-	}
-//what to return if isGood is true or false
-	if (isGood) {
-		console.log("Arrays are the same")
-	} else {
-		console.log("Arrays are not the same...")
-	}
-}
+const eqArrays = require('./eqArrays');
 
-// TEST
-eqArrays([1, 2, 3], [1, 2, 3]); // => should PASS
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅ Assertation Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🔴 Assertation Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+module.exports = assertArraysEqual;
