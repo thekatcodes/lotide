ca4478eaf78d1a17e2f44cc5b4bafd3cdcab94dc
Katie Liu <katieliu59@gmail.com>
2023-01-16 15:16:49 -0500

    Implement Mocha and Chai to refactor code for testing

--- head.js
+++ head.js
@@ -2,7 +2,9 @@ const assertEqual = require('./assertEqual');
 
 const head = function(array) {
     return array[0];
-  };
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+};
+
+module.exports = head;
+// //TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -2,10 +2,3 @@ const assertEqual = require('../assertEqual');
 
 // assertEqual(head([5, 6, 7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Caitlin", "Caitlin");
-assertEqual("Caitlin", "Jennifer");
-assertEqual(22, 22);
-assertEqual(22, 5);
